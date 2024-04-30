2ddcb00ae20b8a7684b8b2cc596871d95ac50c46
Deepak <deepaksharma83407@gmail.com>
2024-04-30 21:38:18 +0530

    project setup

---
+++ api/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("MongoDb is connected succesfully");
+} catch (error) {
+  console.log("error occured during Mongodb connection");
+  
+}
+
+
+app.use(cookieParser())
+app.use(express.json());
+
+
+
+
+app.use((err, req, res, next) => {
+  const statuscode = err.statuscode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statuscode).json({
+    success: false,
+    statuscode,
+    message,
+  });
+});
+
+app.listen(8800, () => {
+  console.log("server is running on port 8800");
+});
